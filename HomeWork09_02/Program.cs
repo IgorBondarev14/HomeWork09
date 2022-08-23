fced0705b8d82c410c8671749d6d8efb4e5b40bd
Igor <garkol@bk.ru>
2022-08-23 18:52:24 +0300

    task two

--- HomeWork09_02/Program.cs
+++ HomeWork09_02/Program.cs
@@ -1,14 +1,20 @@
-﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
 // Даны два неотрицательных числа m и n.
 
 
+long A(long m, long n)
+{
+    if (m == 0)
+        return n + 1;
+    if (n == 0 && m > 0)
+        return A(m - 1, 1);
+    return A(m - 1, A(m, n - 1));
+}
+ 
 
-
-
-
-
-
-Console.Write("Введите первое значение: ");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите второе значение: ");
-int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите первое НЕ отрицательное число: ");
+long m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе НЕ отрицательное число: ");
+long n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(A(m, n));
+ 
